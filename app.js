34eac2876c52cc0e029c83101713e83ef871f5ba
Sujit <sujitkumarborse@gmail.com>
2017-10-11 00:26:32 +0530

    cross rigin access added

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ var _ = require('underscore');
 global.Promise = bluebird;
 global.logger = logger;
 
+app.all("/opd-api/*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    return next();
+});
+
+app.all("/opd-api/*", function(req, res, next) {
+    if (req.method.toLowerCase() !== "options") {
+        return next();
+    }
+    return res.send(204);
+});
+
 mongoose.connect(mongoUrl, err => {
     if (err) {
         logger.error(err);
