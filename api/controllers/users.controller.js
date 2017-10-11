a4b5f85c2cdaf324bd27ded0d0d16724fe0d2738
Sujit <sujitkumarborse@gmail.com>
2017-10-11 23:18:46 +0530

    changes added

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -13,37 +13,39 @@ var options = {
     collectionName: "users"
 };
 schema.pre("save", cuti.counter.getIdGenerator("users", "USER"));
+
+let createToken = function(data) {
+
+    console.log("Data to create after post ");
+
+};
+
+schema.post("save", createToken(this));
+
 var crudder = new SMCrud(schema, "users", options);
 
 var users = mongoose.model('users', schema);
 
 
 let loginUser = function (_req, _res) {
+
     console.log("login data", _req.body);
-
     let data = _req.body;
-
     if (!data || !data.email || !data.password) {
         return _res.json({ status: 'failed', message: "Username/Password is missing." });
     }
-
     users.findOne({ "email": data.email, "password": data.password }, (err, user) => {
-
         if (err) {
             return _res.json({ status: 'failed', message: "Something went wrong. Please tyr again later." });
         };
-
         if (!user) {
             _res.json({ status: 'failed', message: 'Authentication failed. User not found.' });
         } else if (user) {
-
             // check if password matches
             if (user.password != _req.body.password) {
                 _res.json({ status: 'failed', message: 'Authentication failed. Wrong password.' });
             } else {
-
-                // if user is found and password is right
-                // create a token with only our given payload
+                // if user is found and password is right, create a token with only our given payload
                 // remove password from user before creating token
                 delete user['password'];
 
@@ -51,21 +53,13 @@ let loginUser = function (_req, _res) {
                 var token = jwt.sign(user, 'superSecret', {
                     expiresInMinutes: 1440 // expires in 24 hours
                 });
-
                 // return the information including token as JSON
-                user.token = token;
                 _res.json({
                     status: 'success',
-                    user: user
+                    token: token
                 });
             }
-
         }
-
-
-
-
-
     });
 };
 
